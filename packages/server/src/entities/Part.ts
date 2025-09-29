45f7db4b54e04cbb5fb4ec13b2dacaf89a9f5d50
Benoit22324 <sunbenoit384@gmail.com>
2025-09-29 14:37:52 +0200

    Ajout des entités de schéma

---
+++ packages/server/src/entities/Part.ts
@@ -0,0 +1,6 @@
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { parts } from "../schemas";
+
+export type Part = InferSelectModel<typeof parts>;
+
+export type NewPart = InferInsertModel<typeof parts>;
---
+++ packages/server/src/entities/index.ts
@@ -0,0 +1,6 @@
+export * from "./Quiz";
+export * from "./Comment";
+export * from "./Note";
+export * from "./Part";
+export * from "./Run";
+export * from "./User";
