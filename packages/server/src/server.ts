de2533a727158a253484c25c06d15d1731f2c9b2
Benoit22324 <sunbenoit384@gmail.com>
2025-10-09 08:52:51 +0200

    Tentative de résolution prod 2

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -8,7 +8,8 @@ const app = express();
 
 app.use(cors({
     origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://quizquestfront-production.up.railway.app"],
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
 app.use(cookieParser());
 app.use(express.json());
